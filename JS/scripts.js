1a0dcf7b8dd31c89b8634be53b34dd6dec7e4380
BriWins <95152254+BriWins@users.noreply.github.com>
2021-12-17 11:48:33 -0600

    Removed ("") from integers

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,10 +1,10 @@
 let pokemonList = [
-    { name:"Pikachu", height:"0.4", type: ["static","lightningrod"]},
-    { name:"Cubchoo", height:"0.5", type: ["charm","growl"]},
-    { name:"Pidgeot", height:"1.5", type: ["tackle","twister"]},
-    { name:"Wigglytuff", height:"1", type: ["sing","attract"]},
-    { name:"Pichu", height:"0.3", type: ["sweet kiss","charm"]},
-    { name:"Igglybuff", height:"0.3", type: ["cute-charm","friend-guard"]}
+    { name:"Pikachu", height:0.4, type: ["static","lightningrod"]},
+    { name:"Cubchoo", height:0.5, type: ["charm","growl"]},
+    { name:"Pidgeot", height:1.5, type: ["tackle","twister"]},
+    { name:"Wigglytuff", height:1, type: ["sing","attract"]},
+    { name:"Pichu", height:0.3, type: ["sweet kiss","charm"]},
+    { name:"Igglybuff", height:0.3, type: ["cute-charm","friend-guard"]}
 ];
 
 document.write(pokemonList);
