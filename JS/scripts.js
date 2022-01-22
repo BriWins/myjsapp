a43a09a05559c30f18ef9e538e00ccbbc653afea
BriWins <95152254+BriWins@users.noreply.github.com>
2022-01-22 09:30:40 -0600

    Updated scripts.js file
    removed previous array items
    created loadDetails() and loadList() function
    added apiUrl to pokemonList array
    loadList() function rendered outside invoked function

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,105 +1,98 @@
 //Brianna Winston
-//Last updated~ January 20, 2022
+//Last updated~ January 22, 2022
 
                         /* This program demonstrates a simple JS app */
 
 //pokemonList array has been encapsulated in a  "IIFE" repository function to eliminate global variable interference from other javascript files
 
 let repository = (function() {
+let  pokemonList = [];
+let apiUrl = " https://pokeapi.co/api/v2/pokemon/?limit=150 ";
 
-  let pokemonList = [{
-          name: "Pikachu",
-          height: 0.4,
-          type: ["static", "lightningrod"]
-      },
-      {
-          name: "Cubchoo",
-          height: 0.5,
-          type: ["charm", "growl"]
-      },
-      {
-          name: "Pidgeot",
-          height: 1.5,
-          type: ["tackle", "twister"]
-      },
-      {
-          name: "Wigglytuff",
-          height: 1,
-          type: ["sing", "attract"]
-      },
-      {
-          name: "Pichu",
-          height: 0.3,
-          type: ["sweet kiss", "charm"]
-      },
-      {
-          name: "Igglybuff",
-          height: 0.3,
-          type: ["cute-charm", "friend-guard"]
-      },
-      {
-          name: "Diglet",
-          height: 0.2,
-          type: ["growl", "sucker punch"]
-      }
-  ]
+function add(newPokemon) {
+    let error = "Invalid! Input must be an object!";
+    if (typeof newPokemon === "object") {
+        pokemonList.push(newPokemon)
+    } else {
+        return error;
+    }
+}
 
-  //add() function takes input for a new pokemon
+function getAll() {
+    return pokemonList;
+}
 
-  function add(newPokemon) {
-      let error = "Invalid! Input must be an object!";
-      if (typeof newPokemon === "object") {
-          pokemonList.push(newPokemon)
-      } else {
-          return error;
-      }
-  }
-
-//showDetails() iterates through each object in pokemonList array
-
-  function showDetails( pokemon ){
-    console.log( pokemon );
-  }
-
-//addListItem() creates <li> tag and a button with .button--class
-
-  function addListItem(pokemon) {
-      let pokemonList = document.querySelector(".pokemon--list");
-      let listpokemon = document.createElement("li");
-      let button = document.createElement("button");
-      button.innerText = pokemon.name;
-      button.classList.add("button-class");
-      listpokemon.appendChild(button);
-      pokemonList.appendChild(listpokemon);
-
-      button.addEventListener( 'click', function(){ 
-      showDetails( pokemon.name )
-      })
+function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon--list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+    button.addEventListener( 'click', function(){ 
+    showDetails( pokemon.name )
+    })
     }
       
-//getAll() gathers all from repository
- 
-  function getAll() {
-      return pokemonList;
-  }
+function loadList() {
+    return fetch(apiUrl).then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        json.results.forEach(function (item) {
+            let pokemon = {
+                name: item.name,
+                detailsUrl: item.url
+            };
+            add(pokemon);
+        });
+    }).catch(function(e){
+        console.error(e);
+    })
+}
 
-//returns added pokemon input, addListItem(0) function input, and updated repository
- 
-  return {
-      add: add,
-      getAll: getAll,
-      addListItem: addListItem
-  };
+function loadDetails(item){
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+        return response.json();
+    }).then(function (details){
+        item.imgUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+    }).catch(function(e){
+        console.error(e);
+    });
+}
+    
+function showDetails(item) {
+    repository.loadDetails(item).then(function () {
+        console.log(item);
+    });
+}
+        
+return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
+};
 
 })();
 
-//grabs informations from within IFFY functions and displays each object in browswer
-
-repository.getAll().forEach(function(pokemon) {
-
-repository.addListItem(pokemon);
-
+repository.loadList().then(function() {
+    repository.getAll().forEach(function(pokemon) {
+        repository.addListItem(pokemon);
+    });
 }); 
+
+
+
+       
+
+
+      
 
 
 
